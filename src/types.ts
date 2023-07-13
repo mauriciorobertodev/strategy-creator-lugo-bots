38ac0f0021088e0a337fcebfedd65247ff534d1c
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 17:40:49 -0300

    criação do tipo HoldedPlayer

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
+import PlayerContract from "./contracts/player-contract";
+import RegionContract from "./contracts/region-contract";
+
 export type Side = "HOME" | "AWAY";
 
 export type PlayerNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
+
+export type HoldedPlayer = { player: PlayerContract; region: RegionContract | null };
