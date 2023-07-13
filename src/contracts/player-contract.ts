43bc2e2c3a5c1f8a9591550b9157a8e9fc2fd388
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 15:37:03 -0300

    criação do contrato para a classe player

---
+++ src/contracts/player-contract.ts
@@ -0,0 +1,21 @@
+import { PaintContract, Vector2D } from "@mauricioroberto/math-world";
+import { PlayerNumber } from "../types";
+
+export interface PlayerContract {
+    draw(paint: PaintContract): void;
+    getNumber(): PlayerNumber;
+    getRadius(): number;
+    getPosition(): Vector2D;
+    setPosition(position: Vector2D): void;
+    setDirection(direction: Vector2D): void;
+    getInitialPosition(): Vector2D;
+    getDirection(): Vector2D;
+    getInitialDirection(): Vector2D;
+    getCol(): number | null;
+    getRow(): number | null;
+    setColAndRow(col: number, row: number): void;
+    resetColAndRow(): void;
+    hasColAndRow(): boolean;
+    resetPosition(): void;
+    updatePositionByColAndRow(): void;
+}
