271445111dd50d2c349f09ddf76120213a8551d4
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 16:11:37 -0300

    adição da possibilidade de mover o jogador

--- src/loop.ts
+++ src/loop.ts
@@ -1,14 +1,22 @@
 import { MathWorld } from "@mauricioroberto/math-world";
 import { drawField, drawRegions } from "./helpers/draw";
 import global from "./global";
+import { getDistanceBetweenVectors } from "./helpers/math";
 
 const loop = (world: MathWorld) => {
     // VARIÁVEIS
     const paint = world.getPaint();
+    let move_player: number | null = null;
+
+    // SETUP
+    world.onLeftCLick(onLeftClick);
+    world.onMouseUp(onMouseUp);
     paint.cartesian();
 
     // LOOP
     return (): void => {
+        const MOUSE = world.getMousePositionInCartesian();
+
         // DESENHANDO O CAMPO
         drawField(paint);
 
@@ -17,7 +25,36 @@ const loop = (world: MathWorld) => {
 
         // DESENHANDO OS JOGADORES
         global.getPlayers().forEach((player) => player.draw(paint));
+
+        // ATUALIZA POSIÇÃO DO JOGADOR QUE O USUÁRIO ESTÁ SEGURANDO
+        if (move_player) {
+            global
+                .getPlayers()
+                .find((player) => player.getNumber() === move_player)
+                ?.setPosition(MOUSE);
+        }
     };
+
+    // VERIFICANDO SE O CLIQUE É UM JOGADOR PARA MUDAR SUA POSIÇÃO
+    function onLeftClick() {
+        const MOUSE = world.getMousePositionInCartesian();
+
+        // VERIFICANDO A DISTÂNCIA E SETANDO O PLAYER COMO JOGADOR QUE DEVE SER MOVIDO
+        global.getPlayers().find((player) => {
+            const distance = getDistanceBetweenVectors(MOUSE, player.getPosition());
+            if (distance <= player.getRadius() + 15) {
+                move_player = player.getNumber();
+                return player;
+            }
+        });
+    }
+
+    function onMouseUp() {
+        // SE NÃO TIVER JOGADOR SENDO SEGURADO SÓ RETORNA
+        if (!move_player) return;
+
+        move_player = null;
+    }
 };
 
 export default loop;
