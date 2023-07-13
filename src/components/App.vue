0e51f99d0eeb89709650f455f8a8a785b2948eca
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 15:34:50 -0300

    adição do menu

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { FIELD_POINT_CENTER } from "../helpers/constants";
 import { MathWorld } from "@mauricioroberto/math-world";
 import loop from "../loop";
+import Menu from "./Menu.vue";
+
+const isVisibleMenu = ref(false);
 
 onMounted(() => {
     const WORLD = new MathWorld("world");
@@ -20,10 +23,15 @@ onMounted(() => {
     WORLD.loop(loop(WORLD));
     WORLD.start();
 });
+
+function toggleMenu(): void {
+    isVisibleMenu.value = !isVisibleMenu.value;
+}
 </script>
 
 <template>
     <canvas id="world" class="overflow-hidden"></canvas>
+    <Menu v-bind:show-menu="isVisibleMenu" v-on:toggle="toggleMenu()" />
 </template>
 
 <style scoped></style>
