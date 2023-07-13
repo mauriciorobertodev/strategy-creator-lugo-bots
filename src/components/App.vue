f822c1112a7c38f5c0432d331db4caea85746f56
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 14:51:39 -0300

    organização e instalação do tailwindcss

---
+++ src/components/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="h-12 w-12 bg-orange-500">.</div>
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import "./assets/style.css";
+import App from "./components/App.vue";
 
 createApp(App).mount("#app");
