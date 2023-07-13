ed7af864613506ef5eb2a16667ed96a0147ed87c
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 17:43:29 -0300

    alteração para estar sempre atualizando o player segurado e o player abaixo do mouse do usuário

--- src/loop.ts
+++ src/loop.ts
@@ -1,7 +1,8 @@
 import { MathWorld } from "@mauricioroberto/math-world";
-import { drawField, drawRegions } from "./helpers/draw";
+import { drawField, drawPlayers, drawRegions } from "./helpers/draw";
 import global from "./global";
 import { getDistanceBetweenVectors } from "./helpers/math";
+import { getRegionFromPoint } from "./helpers/field";
 
 const loop = (world: MathWorld) => {
     // VARIÁVEIS
@@ -10,6 +11,7 @@ const loop = (world: MathWorld) => {
     // SETUP
     world.onLeftCLick(onLeftClick);
     world.onMouseUp(onMouseUp);
+    world.onMouseMove(onMouseMove);
     paint.cartesian();
 
     // LOOP
@@ -22,31 +24,64 @@ const loop = (world: MathWorld) => {
         // DESENHANDO AS REGIÕES
         drawRegions(paint);
 
-        // DESENHANDO OS JOGADORES E O GOLEIRO
-        global.getPlayers().forEach((player) => player.draw(paint));
-        global.getGoalkeeper().draw(paint);
+        // DESENHANDO OS JOGADORES
+        drawPlayers(paint);
 
         // ATUALIZA POSIÇÃO DO JOGADOR QUE O USUÁRIO ESTÁ SEGURANDO
-        if (global.hasHoldedPlayer()) global.getHoldedPlayer().setPosition(MOUSE);
+        if (global.hasHoldedPlayer()) global.getHoldedPlayer().player.setPosition(MOUSE);
     };
 
-    // VERIFICANDO SE O CLIQUE É UM JOGADOR PARA MUDAR SUA POSIÇÃO
     function onLeftClick() {
-        const MOUSE = world.getMousePositionInCartesian();
-
-        // VERIFICANDO A DISTÂNCIA E SETANDO O PLAYER COMO JOGADOR QUE DEVE SER MOVIDO
-        global.getPlayers().find((player) => {
-            const distance = getDistanceBetweenVectors(MOUSE, player.getPosition());
-            if (distance <= player.getRadius() + 15) {
-                global.setHoldedPlayer(player);
-                return player;
-            }
-        });
+        // SE TIVER UM JOGADOR ABAIXO DO MOUSE, SETAMOS ELE COMO JOGADOR QUE ESTÁ SENDO SEGURADO PELO USUÁRIO
+        if (global.hasPlayerUnderMouse()) {
+            const region = getRegionFromPoint(global.getPlayerUnderMouse().getPosition());
+            global.setHoldedPlayer({ player: global.getPlayerUnderMouse(), region });
+            global.resetPlayerUnderMouse();
+        }
     }
 
     function onMouseUp() {
-        // SE NÃO TIVER JOGADOR SENDO SEGURADO O LIBERTE
-        if (global.hasHoldedPlayer()) global.resetHoldedPlayer();
+        // SE NÃO TIVER JOGADOR SENDO SEGURADO NÃO TEMOS NADA PARA FAZER AQUI
+        if (!global.hasHoldedPlayer()) return;
+
+        const player = global.getHoldedPlayer().player;
+        const region = global.getHoldedPlayer().region;
+
+        // O PLAYER SEGURA ESTÁ EMCIMA DE UMA REGIÃO ENTÃO ELE DEVE FICA NO CENTRO DELA, E HERDAR SUA COLUNA E LINHA
+        if (region) {
+            player.setColAndRow(region.getCol(), region.getRow());
+            player.setPosition(region.getCenter());
+        }
+
+        // SE NÃO TIVER SÓ RESETAMOS A POSIÇÃO DELE PARA IR PARA A LINHA DE JOGADORES
+        if (!region) player.resetPosition();
+
+        // POR FIM REMOVEMOS O PLAYER SENDO SEGURADO POIS O JOGADOR QUE ANTES ESTAVA AGORA JÁ NÃO ESTÁ MAIS SENDO SEGURADO
+        global.resetHoldedPlayer();
+    }
+
+    function onMouseMove() {
+        const MOUSE = world.getMousePositionInCartesian();
+
+        // SE TIVER UM JOGADOR SENDO SEGURADO NÃO FAZ SENTIDO ADICIONARMOS ELE COMO ABAIXO DO MOUSE POIS JÁ O TEMOS NO GLOBAL
+        if (global.hasHoldedPlayer()) {
+            const region = getRegionFromPoint(global.getHoldedPlayer().player.getPosition());
+            global.getHoldedPlayer().region = region;
+            global.resetPlayerUnderMouse();
+            return;
+        }
+
+        // VERIFICANDO A DISTÂNCIA COM O MOUSE
+        const playerUnderMouse = global.getPlayers().find((player) => {
+            const distance = getDistanceBetweenVectors(MOUSE, player.getPosition());
+            if (distance <= player.getRadius()) return player;
+        });
+
+        // SE TIVER UM JOGADOR A BAIXO DO MOUSE SETAMOS ELE NO GLOBAL
+        if (playerUnderMouse) global.setPlayerUnderMouse(playerUnderMouse);
+
+        // SE NÃO RESETAMOS DO GLOBAL PARA NÃO CAUSAR CONFLITOS
+        if (!playerUnderMouse) global.resetPlayerUnderMouse();
     }
 };
 
