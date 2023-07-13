2f41a225898fdad684d9a8df80afc83f6ccbd3dc
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-13 16:06:59 -0300

    adição de função que retorna a distância entre dois pontos

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -8,3 +8,9 @@ export function angleInRadians(a: Vector2D, b?: Vector2D) {
 export function radiansToDegrees(radians: number) {
     return radians * (180 / Math.PI);
 }
+
+export function getDistanceBetweenVectors(a: Vector2D, b: Vector2D): number {
+    const deltaX = a.getX() - b.getX();
+    const deltaY = a.getY() - b.getY();
+    return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+}
