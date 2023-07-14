aaa821e4fa887fa3bcc595fb0c929e321e924ad9
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-14 11:49:59 -0300

    criação da função que retorna se uma região é permita ou não com base no tipo da formação

--- src/helpers/field.ts
+++ src/helpers/field.ts
@@ -2,7 +2,8 @@ import global from "../global";
 import RegionContract from "../contracts/region-contract";
 import Region from "../classes/region";
 import { Vector2D } from "@mauricioroberto/math-world";
-import { FIELD_HEIGHT, FIELD_WIDTH } from "./constants";
+import { AWAY_GOAL_BOTTOM, AWAY_GOAL_TOP, CENTER_FIELD_RADIUS, FIELD_HEIGHT, FIELD_POINT_CENTER, FIELD_WIDTH, GOAL_RADIUS, HOME_GOAL_BOTTOM, HOME_GOAL_TOP } from "./constants";
+import { getDistanceBetweenVectors, getShortPointBetweenLineAndPoint } from "./math";
 
 export function getRegionFromColAndRow(col: number, row: number): RegionContract | null {
     if (col > global.getCols() - 1 || col < 0) return null;
@@ -24,4 +25,26 @@ export function getRegionFromPoint(point: Vector2D): Region | null {
     if (row > global.getRows() - 1 || row < 0) return null;
 
     return new Region(col, row);
+}
+
+export default function isPermittedRegion(region: RegionContract) {
+    // SE O PONTO CENTRAL DA REGIÃO FOR NA ÀREA DO GOL NÃO É PERMITIDO
+    if (global.currentFormationTypeIs("INITIAL_POSITIONS")) {
+        const distanceToHomeGoal = getShortPointBetweenLineAndPoint(HOME_GOAL_TOP, HOME_GOAL_BOTTOM, region.getCenter()).distance;
+        const distanceToAwayGoal = getShortPointBetweenLineAndPoint(AWAY_GOAL_TOP, AWAY_GOAL_BOTTOM, region.getCenter()).distance;
+
+        if (distanceToHomeGoal < GOAL_RADIUS || distanceToAwayGoal < GOAL_RADIUS) return false;
+    }
+
+    // NA FORMAÇÃO INICIAL O CÍRCULO DO CENTRO NÃO PODE SER USADO, NEM O CAMPO ADVERSÁRIO OU A LINHA DO CENTRO
+    if (global.currentFormationTypeIs("INITIAL_POSITIONS")) {
+        const distanceToCenterPoint = getDistanceBetweenVectors(region.getCenter(), FIELD_POINT_CENTER);
+        if (distanceToCenterPoint <= CENTER_FIELD_RADIUS) return false;
+
+        const centerX = region.getCenter().getX();
+        if (global.isHomeSide() && centerX >= FIELD_WIDTH / 2) return false;
+        if (global.isAwaySide() && centerX <= FIELD_WIDTH / 2) return false;
+    }
+
+    return true;
 }
