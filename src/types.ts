cbdcbc7b54026bbb16cfd7798816871779863013
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-14 12:44:56 -0300

    criação de tipos referentes a posições dos jogadores

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,14 @@ export type Side = "HOME" | "AWAY";
 
 export type PlayerNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
 
+export type PlayerNumberWithoutGoalkeeper = Exclude<PlayerNumber, 1>;
+
+export type PlayerPosition = { col: number | null; row: number | null };
+
 export type HoldedPlayer = { player: PlayerContract; region: RegionContract | null; is_permitted_region: boolean };
 
 export type FormationType = "FREE" | "INITIAL_POSITIONS" | "ZONE";
+
+export type FormationFullExport = { [key in PlayerNumber]: PlayerPosition };
+
+export type FormationCommonExport = { [key in PlayerNumberWithoutGoalkeeper]: PlayerPosition };
