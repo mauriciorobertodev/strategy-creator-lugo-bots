94a3e3fd0ad0202059fffd0d9523037c3bdfa431
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-14 11:49:33 -0300

    adição de funções para encontrar o angulo entre dois vetores e o ponto mais próximo de uma linha

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -23,3 +23,37 @@ export function getDirectionToVector(a: Vector2D, b: Vector2D): Vector2D {
 
     return new Vector2D(cos, sin);
 }
+
+export function getAngleBetweenVectorsInRadians(a: Vector2D, b: Vector2D) {
+    return Math.atan2(b.getY() - a.getY(), b.getX() - a.getX());
+}
+
+export function getShortPointBetweenLineAndPoint(startLine: Vector2D, endLine: Vector2D, point: Vector2D): { distance: number; point: Vector2D; angle: number } {
+    const A = point.getX() - startLine.getX();
+    const B = point.getY() - startLine.getY();
+    const C = endLine.getX() - startLine.getX();
+    const D = endLine.getY() - startLine.getY();
+
+    const dot = A * C + B * D;
+    const len_sq = C * C + D * D;
+    let param = -1;
+    if (len_sq != 0) param = dot / len_sq;
+
+    let xx, yy;
+
+    if (param < 0) {
+        xx = startLine.getX();
+        yy = startLine.getY();
+    } else if (param > 1) {
+        xx = endLine.getX();
+        yy = endLine.getY();
+    } else {
+        xx = startLine.getX() + param * C;
+        yy = startLine.getY() + param * D;
+    }
+
+    const dx = point.getX() - xx;
+    const dy = point.getY() - yy;
+    const shortPoint = new Vector2D(xx, yy);
+    return { distance: Math.sqrt(dx * dx + dy * dy), point: shortPoint, angle: getAngleBetweenVectorsInRadians(shortPoint, point) };
+}
