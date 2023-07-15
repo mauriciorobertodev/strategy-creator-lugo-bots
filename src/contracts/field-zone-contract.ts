484fa31544074e11bce27c8122b1572c9a26c497
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-15 10:42:04 -0300

    contrato para zona de campo

---
+++ src/contracts/field-zone-contract.ts
@@ -0,0 +1,18 @@
+import { Vector2D } from "@mauricioroberto/math-world";
+import { Color } from "../types";
+
+export default interface FieldZoneContract {
+    getName(): string;
+    getColor(): Color;
+    getStartCol(): number;
+    getStartRow(): number;
+    getEndCol(): number;
+    getEndRow(): number;
+    getCenter(): Vector2D;
+    getWidth(): number;
+    getHeight(): number;
+    getTopLeft(): Vector2D;
+    getTopRight(): Vector2D;
+    getBottomLeft(): Vector2D;
+    getBottomRight(): Vector2D;
+}
