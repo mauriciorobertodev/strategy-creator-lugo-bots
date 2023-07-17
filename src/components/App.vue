0fb6b789dae7a7fdfaacebc880a7f847ae47f161
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-17 12:32:55 -0300

    adição dos modais

--- src/components/App.vue
+++ src/components/App.vue
@@ -4,14 +4,20 @@ import { FIELD_POINT_CENTER } from "../helpers/constants";
 import { MathWorld } from "@mauricioroberto/math-world";
 import loop from "../loop";
 import Menu from "./Menu.vue";
+import NewStrategyModal from "./NewStrategyModal.vue";
+import ChangeStrategyModal from "./ChangeStrategyModal.vue";
+import DeleteStrategyModal from "./DeleteStrategyModal.vue";
 
 const isVisibleMenu = ref(false);
+const isVisibleNewStrategyModal = ref(false);
+const isVisibleChangeStrategyModal = ref(false);
+const isVisibleDeleteStrategyModal = ref(false);
 
 onMounted(() => {
     const WORLD = new MathWorld("world");
 
     WORLD.default();
-    WORLD.drawInformation(false);
+    WORLD.drawInformation(true);
     WORLD.setCameraMaxZoom(1000);
     WORLD.setCameraMinZoom(1);
     WORLD.drawAxis(false);
@@ -24,14 +30,30 @@ onMounted(() => {
     WORLD.start();
 });
 
-function toggleMenu(): void {
-    isVisibleMenu.value = !isVisibleMenu.value;
-}
+const openNewStrategyModal = () => {
+    isVisibleNewStrategyModal.value = true;
+    isVisibleMenu.value = false;
+};
+const openChangeStrategyModal = () => {
+    isVisibleChangeStrategyModal.value = true;
+    isVisibleMenu.value = false;
+};
+const openDeleteStrategyModal = () => {
+    isVisibleDeleteStrategyModal.value = true;
+    isVisibleMenu.value = false;
+};
+const toggleMenu = () => (isVisibleMenu.value = !isVisibleMenu.value);
+const closeNewStrategyModal = () => (isVisibleNewStrategyModal.value = false);
+const closeChangeStrategyModal = () => (isVisibleChangeStrategyModal.value = false);
+const closeDeleteStrategyModal = () => (isVisibleDeleteStrategyModal.value = false);
 </script>
 
 <template>
     <canvas id="world" class="overflow-hidden"></canvas>
-    <Menu v-bind:show-menu="isVisibleMenu" v-on:toggle="toggleMenu()" />
+    <Menu v-bind:show-menu="isVisibleMenu" v-on:toggle="toggleMenu()" v-on:open-new-strategy-modal="openNewStrategyModal()" v-on:open-change-strategy-modal="openChangeStrategyModal()" v-on:open-delete-strategy-modal="openDeleteStrategyModal()" />
+    <NewStrategyModal v-bind:showModal="isVisibleNewStrategyModal" v-on:close="closeNewStrategyModal()" />
+    <ChangeStrategyModal v-bind:showModal="isVisibleChangeStrategyModal" v-on:close="closeChangeStrategyModal()" />
+    <DeleteStrategyModal v-bind:showModal="isVisibleDeleteStrategyModal" v-on:close="closeDeleteStrategyModal()" />
 </template>
 
 <style scoped></style>
