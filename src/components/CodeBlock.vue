f5a81d9cfc04786cd437ff12dd84d5159fda0c63
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-27 18:05:32 -0300

    componente para code highlight

---
+++ src/components/CodeBlock.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps(["code", "language"]);
+</script>
+
+<template>
+    <div class="not-prose relatve">
+        <div class="relative">
+            <div class="absolute z-50 w-20 h-4 top-12 left-4 mac"></div>
+            <pre class="relative">
+                <code  ref="code" :class="`rounded-lg shadow-2xl !p-0 !m-0 language-${language} `">{{ code }}</code>
+            </pre>
+        </div>
+    </div>
+</template>
