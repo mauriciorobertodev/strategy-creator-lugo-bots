1a6a7d793ca3bbc021493e3b0c6d03d77eccf522
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-07-27 18:05:07 -0300

    adição da font inter e plugin de tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/typography")],
 };
