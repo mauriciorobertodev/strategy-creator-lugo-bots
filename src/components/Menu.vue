e5f4951a975355ce238e3b373734d00b1bb8e450
Vinicius-Marques6 <marquesv473@gmail.com>
2024-09-20 20:26:19 -0400

    Fix: muda ícone do menu e desabilita ações sem função

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -117,19 +117,15 @@ const renameFormation = (formation: FormationContract, name: string) => {
                                             </svg>
                                         </div>
                                     </button>
-                                    <!-- botão de exclusão -->
+                                    <!-- menu de ações -->
                                     <DropdownMenu.Root>
                                         <DropdownMenu.Trigger as-child>
                                             <button
                                                 type="button"
                                                 class="flex items-center justify-center w-12 h-10 text-gray-500 uppercase border border-gray-200 rounded-md hover:border-gray-500 hover:text-white hover:bg-gray-500"
                                             >
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                                    <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                                    />
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                                                 </svg>
                                             </button>
                                         </DropdownMenu.Trigger>
@@ -141,18 +137,20 @@ const renameFormation = (formation: FormationContract, name: string) => {
                                                 :side-offset="5"
                                             >
                                                 <DropdownMenu.Item
-                                                    class="group text-sm leading-none text-gray-700 rounded flex items-center py-1.5 px-2 relative select-none outline-none data-[highlighted]:bg-blue-500 data-[highlighted]:text-white"
+                                                    class="group text-sm leading-none text-gray-700 rounded flex items-center py-1.5 px-2 relative select-none outline-none data-[highlighted]:bg-blue-500 data-[highlighted]:text-white data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
                                                     @select="delayMethod(edit)"
                                                 >
                                                     Renomear
                                                 </DropdownMenu.Item>
                                                 <DropdownMenu.Item
-                                                    class="group text-sm leading-none text-gray-700 rounded flex items-center py-1.5 px-2 relative select-none outline-none data-[highlighted]:bg-blue-500 data-[highlighted]:text-white"
+                                                    class="group text-sm leading-none text-gray-700 rounded flex items-center py-1.5 px-2 relative select-none outline-none data-[highlighted]:bg-blue-500 data-[highlighted]:text-white data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+                                                    disabled
                                                 >
                                                     Copiar formação
                                                 </DropdownMenu.Item>
                                                 <DropdownMenu.Item
-                                                    class="group text-sm leading-none text-gray-700 rounded flex items-center py-1.5 px-2 relative select-none outline-none data-[highlighted]:bg-blue-500 data-[highlighted]:text-white"
+                                                    class="group text-sm leading-none text-gray-700 rounded flex items-center py-1.5 px-2 relative select-none outline-none data-[highlighted]:bg-blue-500 data-[highlighted]:text-white data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+                                                    disabled
                                                 >
                                                     Colar formação
                                                 </DropdownMenu.Item>
